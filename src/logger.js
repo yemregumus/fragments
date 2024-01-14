24b6233ca320ea379c6236d703c3a22d53fad9b3
Yunus Gumus <ygumus@myseneaca.ca>
2024-01-14 12:22:32 -0500

    Environment variables print added to logger.js

--- src/logger.js
+++ src/logger.js
@@ -6,6 +6,11 @@ const options = { level: process.env.LOG_LEVEL || 'info' };
 // If we're doing `debug` logging, make the logs easier to read
 if (options.level === 'debug') {
   // https://github.com/pinojs/pino-pretty
+
+  console.log('===========================================');
+  console.log('Environment Variables:');
+  console.log(process.env);
+  console.log('===========================================');
   options.transport = {
     target: 'pino-pretty',
     options: {
