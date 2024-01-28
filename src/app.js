a80d4251b5bff4fbd1d82521c8cf44d72bba1ce7
Yunus Gumus <ygumus@myseneaca.ca>
2024-01-28 11:19:12 -0500

    got rid of unused variables

--- src/app.js
+++ src/app.js
@@ -7,10 +7,6 @@ const compression = require('compression');
 const passport = require('passport');
 const authenticate = require('./auth');
 
-// author and version from our package.json file
-// TODO: make sure you have updated your name in the `author` section
-const { author, version } = require('../package.json');
-
 const logger = require('./logger');
 const pino = require('pino-http')({
   // Use our default logger instance, which is already configured
