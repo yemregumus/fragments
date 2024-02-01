aa889bb7196a6b5dc9fa699190b3b8f591137f2e
Yunus Gumus <ygumus@myseneaca.ca>
2024-02-01 12:54:17 -0500

    coverage report changes recorded

--- src/app.js
+++ src/app.js
@@ -26,24 +26,6 @@ app.use(helmet());
 // Use CORS middleware so we can make requests across origins
 app.use(cors());
 
-// Define a simple health check route. If the server is running
-// we'll respond with a 200 OK.  If not, the server isn't healthy.
-/*app.get('/', (req, res) => {
-  // Clients shouldn't cache this response (always request it fresh)
-  // See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching#controlling_caching
-  res.setHeader('Cache-Control', 'no-cache');
-
-  // Send a 200 'OK' response with info about our repo
-  res.status(200).json({
-    status: 'ok',
-    author,
-    // TODO: change this to use your GitHub username!
-    githubUrl: 'https://github.com/yemregumus/fragments',
-    version,
-  });
-});
-*/
-
 // Use gzip/deflate compression middleware
 app.use(compression());
 
