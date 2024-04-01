cf1a82bd83ee61e61da1efe848cdcb26b7cf3fea
yemregumus <ygumus@myseneca.ca>
2024-04-01 11:41:39 -0400

    dynamoDB table created, local tests with minio implemented, put route finalized with unit tests, lab10 integration tests added.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 // src/routes/index.js
 
 const express = require('express');
+const contentType = require('content-type');
+const { Fragment } = require('../../src/model/fragment');
 
 const { authenticate } = require('../../src/authorization');
 
@@ -33,6 +35,24 @@ router.get('/', (req, res) => {
       hostname: hostname(),
     })
   );
+
+  // Support sending various Content-Types on the body up to 5M in size
+  const rawBody = () =>
+    express.raw({
+      inflate: true,
+      limit: '5mb',
+      type: (req) => {
+        // See if we can parse this content type. If we can, `req.body` will be
+        // a Buffer (e.g., `Buffer.isBuffer(req.body) === true`). If not, `req.body`
+        // will be equal to an empty Object `{}` and `Buffer.isBuffer(req.body) === false`
+        const { type } = contentType.parse(req);
+        return Fragment.isSupportedType(type);
+      },
+    });
+
+  // Use a raw body parser for POST, which will give a `Buffer` Object or `{}` at `req.body`
+  // You can use Buffer.isBuffer(req.body) to test if it was parsed by the raw body parser.
+  router.post('/v1/fragments', rawBody(), require('./api/post'));
 });
 
 module.exports = router;
