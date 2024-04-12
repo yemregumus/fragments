2d061e451f07c7c7c3a0c1d5b3cd343eb21523a3
yemregumus <ygumus@myseneca.ca>
2024-04-12 17:03:06 -0400

    removed unused library

--- tests/unit/fragment.test.js
+++ tests/unit/fragment.test.js
@@ -1,5 +1,4 @@
 const { Fragment } = require('../../src/model/fragment');
-const MarkdownIt = require('markdown-it');
 
 // Wait for a certain number of ms.Returns a Promise.
 const wait = async (ms = 1000) => new Promise((resolve) => setTimeout(resolve, ms));
